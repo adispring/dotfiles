b6bd7c400d1a4cdb977b1f2894e45be0624ce4af
adispring <sunnyadi@163.com>
2018-04-22 16:59:06 +0800

    intergration with oh-my-zsh
    1. change .dotfiles global env variable $ZSH to $DOTFILES.
    2. can using oh-my-zsh's plugins
    3. remove atom.symlink directory

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,21 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias clls='clear && ls'
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -lah'
+alias v='vim'
+alias grep="grep --color=auto"
+
+# adi's alias
+alias gpo='git push origin'
+alias groot='cd $(git rev-parse --show-toplevel)'
+alias dirhide='defaults write com.apple.finder AppleShowAllFiles -boolean false ; killall Finder'
+alias dirshow='defaults write com.apple.finder AppleShowAllFiles -boolean true ; killall Finder'
+alias thrift_parser="node ~/JavaScript/Github/thrift-parser/bin/thrift-parser.js"
+
+# remap terminal tools with customized arguments
+alias emacs="emacs -nw"
+alias ag='ag --path-to-ignore ~/.agignore'
+
